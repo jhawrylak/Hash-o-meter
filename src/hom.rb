83832851d531f9d263bda00fc83a036d9b65514e
Jarrett Hawrylak <jjhawrylak+git@gmail.com>
2012-02-03 23:07:56 -0500

    new directory structure, config code rework

--- src/hom.rb
+++ src/hom.rb
@@ -1,12 +1,12 @@
-require './lib/HashometerHelper.rb'
+require File.join(File.dirname(__FILE__),'/lib/HashometerHelper.rb')
 include HashometerHelper
 
-configfile = File.open('config/config.rb', "r")
+configfile = File.open(File.join(File.dirname(__FILE__),'../config/config.rb'), "r")
 config = {}
 while !configfile.eof?
   line = configfile.readline.chomp
   next if line[0] =='#'
-  line = configfile.readline.chomp.split("=").collect { |val| val.strip.gsub('"','') }
+  line = line.split("=").collect { |val| val.strip.gsub('"','') }
   config[line[0].to_sym] = line[1]
 end
 testconfig(config,:username)
